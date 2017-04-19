f91fb490a74518774a3d9bc928e2f2a88c74e309
Calamity Lorenzo <lawrence_paul@hotmail.co.uk>
2017-04-19 20:22:34 +0100

    Add project files.

---
+++ FoundationNetCore/gulpfile.js
@@ -0,0 +1,39 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    rimraf = require('rimraf'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    copy = require('gulp-copy'),
+    fileExists = require('file-exists');
+
+
+var paths = {
+    webroot: "./wwwroot/"
+}
+
+paths.js = paths.webroot + "js/**/*.js";
+paths.minJs = paths.webroot + "js/**/*.min.js";
+paths.css = paths.webroot + "css/**/*.css";
+paths.minCss = paths.webroot + "css/**/*.min.css";
+paths.concatJsDest = paths.webroot + "js/site.min.js";
+paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.libJs = paths.webroot + "lib/"
+gulp.task('default', function () {
+    // place code for your default task here
+
+});
+
+function getFile(fileName) {
+    fileExists(fileName, { root: paths.libJs }, (err, exists) => { return err; });
+}
+
+gulp.task('js:foundation', function () {
+    var s = getFile("foundation.min.js");
+    console.log(s);
+});
